46670eeaf443b5ec32d165bb1d4330fd71161565
Gaurav Gupta <gouravgupta840@gmail.com>
2022-12-31 13:09:48 +0530

    added theme and code for refreshing the app

--- src/index.tsx
+++ src/index.tsx
@@ -12,10 +12,22 @@ root.render(
   </React.StrictMode>
 );
 
+// https://react-redux.js.org/tutorials/quick-start
+
+setTimeout(() => {
+  // Refreshing the website once, just because of a minor glitch fix
+  let reloadedOnce = window.localStorage.getItem("loadedOnce");
+  if (reloadedOnce !== "true") {
+    window.localStorage.setItem("loadedOnce", "true");
+    document.location.reload();
+  }
+  setTimeout(() => window.localStorage.setItem("loadedOnce", "false"), 1000);
+}, 100);
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.unregister(); // TODO: please change this to .register() when you will build the application
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
