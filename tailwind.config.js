3906fff77a34c119aeab66db4801a6fa696ae4c5
Ryanhustler420 <gouravgupta840@gmail.com>
2023-01-01 16:18:57 +0530

    re added tailwind and removed bootstrap

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./public/**/*.html", "./src/**/*.tsx"],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
